d17fd6140968e1143746969cf03656c4de03116e
Marcin Wawrzak <korni888@yahoo.pl>
2019-09-07 12:39:40 +0200

    Mix2

---
+++ src/com/sda/podstawy/błędy/trst.java
@@ -0,0 +1,11 @@
+public class jvm{
+
+    public static void main(String args[]){
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int suma = a +b;
+        System.out.println(suma);
+
+    }
+
+}
